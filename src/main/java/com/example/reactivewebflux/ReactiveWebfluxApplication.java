9da186d70c46211a86d19adddd1ee8dd7f64ff8a
vAdyar <vikas.adyar@intertrust.com>
2020-05-02 11:16:09 +0530

    Clearing rsocket code.

--- src/main/java/com/example/reactivewebflux/ReactiveWebfluxApplication.java
+++ src/main/java/com/example/reactivewebflux/ReactiveWebfluxApplication.java
@@ -1,15 +1,10 @@
 package com.example.reactivewebflux;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
@@ -18,10 +13,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
+@Repository
+interface UserCrudRepository extends ReactiveCrudRepository<User, String> {
+
+}
 
 @SpringBootApplication
 public class ReactiveWebfluxApplication {
@@ -77,10 +73,5 @@ class User {
     private String fName;
     private String lName;
     private int age;
-
-}
-
-@Repository
-interface UserCrudRepository extends ReactiveCrudRepository<User, String> {
 
 }
